3e8ed9d088a83f2ccaa189c9f0275979bfca312d
devops979 <devops979@gmail.com>
2025-07-08 12:05:19 +0530

    Update outputs.tf

--- terraform/modules/ses/outputs.tf
+++ terraform/modules/ses/outputs.tf
@@ -1,1 +1,4 @@
-output "identity_arn" { value = aws_ses_email_identity.this.arn }
+output "identity_arn" {
+  description = "The Amazon Resource Name (ARN) of the verified email identity in AWS SES, used for sending emails."
+  value       =  aws_ses_email_identity.this.arn
+}
