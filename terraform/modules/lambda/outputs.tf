fe5b1070d12d479ade4f300de1bfebac71882e92
devops979 <devops979@gmail.com>
2025-07-08 12:01:53 +0530

    Update outputs.tf

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,1 +1,4 @@
-output "lambda_arn" { value = aws_lambda_function.this.arn }
+output "lambda_arn" {
+  value       = aws_lambda_function.this.arn
+  description = "The Amazon Resource Name (ARN) of the deployed AWS Lambda function."
+}
