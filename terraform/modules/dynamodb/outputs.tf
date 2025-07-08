3d89de6b8590ce43c0c7b4b204bad26f51be19a0
devops979 <devops979@gmail.com>
2025-07-08 12:19:38 +0530

    Update outputs.tf

--- terraform/modules/dynamodb/outputs.tf
+++ terraform/modules/dynamodb/outputs.tf
@@ -1,1 +1,4 @@
-output "table_arn" { value = aws_dynamodb_table.this.arn }
+output "table_arn" {
+  description = "The Amazon Resource Name (ARN) of the DynamoDB table created, used for referencing the table in IAM policies or other AWS resources."
+  value       = aws_dynamodb_table.this.arn
+}
