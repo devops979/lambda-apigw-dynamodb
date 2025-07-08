0bad08ac114e220027ee2a3d23c1766add78da0d
devops979 <devops979@gmail.com>
2025-07-08 12:16:49 +0530

    Update outputs.tf

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,3 +1,4 @@
 output "role_arn" {
-  value = aws_iam_role.lambda_exec.arn
+  description = "The Amazon Resource Name (ARN) of the IAM role assigned to the Lambda function, used for execution permissions."
+  value       = aws_iam_role.lambda_exec.arn 
 }
