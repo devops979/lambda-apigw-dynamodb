ac640dd1d130e16f2106cc9e67b3d21ff720c29a
devops979 <devops979@gmail.com>
2025-07-08 11:58:48 +0530

    Update variables.tf

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,5 +1,24 @@
-variable "function_name" {}
-variable "role_arn" {}
-variable "handler_file" {}
-variable "table_name" {}
-variable "notify_email" {}
+variable "function_name" {
+  type        = string
+  description = "The name of the AWS Lambda function to be created or managed."
+}
+
+variable "role_arn" {
+  type        = string
+  description = "The ARN of the IAM role that the Lambda function will assume during execution."
+}
+
+variable "handler_file" {
+  type        = string
+  description = "The file name and path of the Lambda function handler, typically in the format 'file_name.function_handler'."
+}
+
+variable "table_name" {
+  type        = string
+  description = "The name of the DynamoDB table that the Lambda function will interact with."
+}
+
+variable "notify_email" {
+  type        = string
+  description = "The email address to receive notifications, such as alerts or updates related to the Lambda function."
+}
