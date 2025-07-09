e1547fad2f525030604c2beb0e5ffd5e37c0527e
devops979 <devops979@gmail.com>
2025-07-09 14:50:37 +0530

    Update variables.tf

--- terraform/modules/ses/variables.tf
+++ terraform/modules/ses/variables.tf
@@ -1,4 +1,5 @@
-variable "email_identity" {
-  type        = string
-  description = "The verified email identity used for sending emails via AWS SES (Simple Email Service)."
+variable "email_identities" {
+  type        = list(string)
+  description = "List of email addresses to verify in SES"
 }
+
